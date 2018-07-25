4a40d85cb6194ca0df7413fad76f263b541ac976
Jeffrey Jiang <anon0503@gmail.com>
2018-07-25 23:57:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function lowerCaseDrivers(drivers){
 }
 
 function nameToAttributes(drivers){
-    drivers.map(functon createnameobject(drivers){
+    drivers.map(function createnameobject(drivers){
     let newname = drivers.split(" ");
     let firstName = newname[0];
     let lastName = newname[1];
